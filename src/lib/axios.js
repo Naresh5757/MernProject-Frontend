02684940f299778b2085fa13feddd027e679f8eb
Naresh5757 <nareshsai57575@gmail.com>
2025-09-09 17:52:48 +0530

    render api applied

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstances=axios.create({
     // baseURL:"https://mernproject-f7fp.onrender.com/api",
-    baseURL:"http://localhost:5000/api",
+    // baseURL:"http://localhost:5000/api",
+    baseURL:"https://mernprojectss-2.onrender.com/api",
     withCredentials:true,//send cookies to the server
 })
 
